d43e959a97bac8d9441b27a06004ad5600ba4187
rakshith <rakshit.s.gowda@gmail.com>
2022-11-09 09:45:34 +0530

    dynamic content insertion

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
+	"time"
 )
 
 // handler function
@@ -10,13 +12,25 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hi there! We are topot."))
 }
 
+func displayTime(w http.ResponseWriter, r *http.Request) {
+	// 1. Get the time
+	localTime := time.Now().Format("3:04:50PM")
+	// 2. read in the template file
+	ts, _ := template.ParseFiles("./ui/html/display.time.tmpl")
+	// 3. substitute from template usisn-> (template Engine)
+	ts.Execute(w, localTime)
+}
+
 func main() {
 	// create a new serveMux
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 
+	// URL for Time
+	mux.HandleFunc("/time", displayTime)
+
 	// create a file server
-	fileServer := http.FileServer(http.Dir("./static/"))
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	// create URL mapping to static directory
 	mux.Handle("/resource/", http.StripPrefix("/resource", fileServer))
 
