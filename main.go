0b5f5995a6f03036adca5d4221fd3c695f8fb77b
rakshith <rakshit.s.gowda@gmail.com>
2022-11-09 08:21:50 +0530

    static resouce hanlefunc fileserver

--- main.go
+++ main.go
@@ -14,6 +14,12 @@ func main() {
 	// create a new serveMux
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+
+	// create a file server
+	fileServer := http.FileServer(http.Dir("./static/"))
+	// create URL mapping to static directory
+	mux.Handle("/resource/", http.StripPrefix("/resource", fileServer))
+
 	// create a web server
 	log.Println("starting server on port :4000")
 	err := http.ListenAndServe(":4000", mux)
