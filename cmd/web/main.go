70bf8b4c3a35a31c0bc7111f487baa104e0e6b5d
rakshith <rakshit.s.gowda@gmail.com>
2023-11-26 09:42:37 +0530

    longtime

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -16,9 +16,19 @@ func displayTime(w http.ResponseWriter, r *http.Request) {
 	// 1. Get the time
 	localTime := time.Now().Format("3:04:50PM")
 	// 2. read in the template file
-	ts, _ := template.ParseFiles("./ui/html/display.time.tmpl")
+	ts, _ := template.ParseFiles("./ui/html/display.time.page.tmpl")
 	// 3. substitute from template usisn-> (template Engine)
 	ts.Execute(w, localTime)
+}
+
+// show the form to the user
+func getValues() {
+
+}
+
+// compute and display the area
+func calculateArea() {
+
 }
 
 func main() {
@@ -29,10 +39,13 @@ func main() {
 	// URL for Time
 	mux.HandleFunc("/time", displayTime)
 
+	mux.HandleFunc("/area-calculator", getValues)
+	mux.HandleFunc("/are-calculator-2", calculateArea)
+
 	// create a file server
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	// create URL mapping to static directory
-	mux.Handle("/resource/", http.StripPrefix("/resource", fileServer))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	// create a web server
 	log.Println("starting server on port :4000")
